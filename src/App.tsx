fc860f8dc2cd1135923b28f93f3b179eaa41b5ea
Mohamed Kaddour <kadnakamo@gmail.com>
2024-12-12 16:07:37 +0100

    Quest4

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import PokemonCard from "./components/PokemonCard";
 
 import "./App.css";
-import MyTitle from "./components/MyTitle";
+//import MyTitle from "./components/MyTitle";
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function App() {
   return (
     <div>
-      <MyTitle />
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]} />
     </div>
   );
 }
