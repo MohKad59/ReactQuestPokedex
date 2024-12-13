d0eae39ff8dbd34473bbdeaf83fff33f6543af1d
Mohamed Kaddour <kadnakamo@gmail.com>
2024-12-13 14:28:32 +0100

    Quest7

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
+import { useState } from "react";
 import PokemonCard from "./components/PokemonCard";
-
 import "./App.css";
-//import MyTitle from "./components/MyTitle";
 
 const pokemonList = [
   {
@@ -10,14 +9,42 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[1]} />
+      <nav>
+        {pokemonList.map((pokemon) => (
+          <button
+            key={pokemon.name}
+            type="button"
+            onClick={() => setPokemonIndex(pokemonList.indexOf(pokemon))}
+          >
+            {pokemon.name}
+          </button>
+        ))}
+      </nav>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
     </div>
   );
 }
